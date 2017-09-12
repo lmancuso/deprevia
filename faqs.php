30103ed5212f58b3c8cbc578feeb6b8d8c3a2036
Fullstack <catalina-delgado@outlook.com>
2017-09-11 21:11:58 -0300

    agrego lorem ipsum a faqs

--- faqs.php
+++ faqs.php
@@ -20,15 +20,15 @@
       <ul class="collapsible" data-collapsible="accordion">
     <li>
       <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita mollitia facilis deserunt placeat nesciunt itaque velit, consequatur consectetur, dolorem aliquid voluptas! Quia excepturi odio pariatur ipsam sapiente ea. Excepturi, deleniti!</span></div>
     </li>
     <li>
       <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
-      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita mollitia facilis deserunt placeat nesciunt itaque velit, consequatur consectetur, dolorem aliquid voluptas! Quia excepturi odio pariatur ipsam sapiente ea. Excepturi, deleniti!</span></div>
     </li>
     <li>
       <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita mollitia facilis deserunt placeat nesciunt itaque velit, consequatur consectetur, dolorem aliquid voluptas! Quia excepturi odio pariatur ipsam sapiente ea. Excepturi, deleniti!</span></div>
     </li>
   </ul>
 
