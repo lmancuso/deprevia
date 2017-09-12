36953e8f0e5b56996cf7f294e01567fb6500319e
Fullstack <catalina-delgado@outlook.com>
2017-09-11 21:01:09 -0300

    cambios en login

--- login.php
+++ login.php
@@ -21,36 +21,49 @@
       <img class="responsive-img" style="width: 250px;" src="https://i.imgur.com/ax0NCsK.gif" />
       <div class="section"></div>
 
-      <h5 class="indigo-text">Please, login into your account</h5>
+      <h5 class="indigo-text">Inicia sesión con tu cuenta</h5>
       <div class="section"></div>
 
       <div class="container">
         <div class="z-depth-1 grey lighten-4 row" style="display: inline-block; padding: 32px 48px 0px 48px; border: 1px solid #EEE;">
 
           <form class="col s12" method="post">
-            <div class='row'>
-              <div class='col s12'>
-              </div>
-            </div>
+            
 
             <div class='row'>
               <div class='input-field col s12'>
                 <input class='validate' type='email' name='email' id='email' />
-                <label for='email'>Enter your email</label>
+                <label for='email'>Ingresa tu Email</label>
               </div>
             </div>
 
             <div class='row'>
               <div class='input-field col s12'>
                 <input class='validate' type='password' name='password' id='password' />
-                <label for='password'>Enter your password</label>
+                <label for='password'>Ingresa tu Password</label>
               </div>
-              <label style='float: right;'>
-								<a class='pink-text' href='#!'><b>Forgot Password?</b></a>
-							</label>
+              
+              <div class="row">
+              <label style='float: center;'>
+                Recordar
+              </label>
+              <!-- Switch -->
+                <div class="switch">
+                  <label>
+                    Off
+                    <input type="checkbox">
+                    <span class="lever"></span>
+                    On
+                  </label>
+                </div>
+                <label style='float: center;'>
+                <br>
+                <a class='pink-text' href='#!'><b>Perdí mi clave</b></a>
+                </label>
+              </div>
+
             </div>
 
-            <br />
             <center>
               <div class='row'>
                 <button type='submit' name='btn_login' class='col s12 btn btn-large waves-effect indigo'>Login</button>
@@ -59,7 +72,7 @@
           </form>
         </div>
       </div>
-      <a href="#!">Create account</a>
+      <a href="register.php">Crear cuenta</a>
     </center>
 
     <div class="section"></div>
