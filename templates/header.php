5834ce2c593d38a77e070795a6806bbe7e71efe2
Fullstack <catalina-delgado@outlook.com>
2017-09-11 21:09:36 -0300

    agrego opciones al menu

--- templates/header.php
+++ templates/header.php
@@ -2,11 +2,17 @@
   <nav class="light-blue lighten-1" role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Logo</a>
       <ul class="right hide-on-med-and-down">
-        <li><a href="#">Navbar Link</a></li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="faqs.php">FAQs</a></li>
+        <li><a href="register.php">Registro</a></li>
+        <li><a href="login.php">Login</a></li>
       </ul>
 
       <ul id="nav-mobile" class="side-nav">
-        <li><a href="#">Navbar Link</a></li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="faqs.php">FAQs</a></li>
+        <li><a href="register.php">Registro</a></li>
+        <li><a href="login.php">Login</a></li>
       </ul>
       <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
