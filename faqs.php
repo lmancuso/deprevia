d2cf485c30eaef2a42dddaec74f5385c8d75d448
Fullstack <fabiancrail.com>
2017-09-15 18:37:11 -0300

    se arregla el faqs

--- faqs.php
+++ faqs.php
@@ -19,30 +19,32 @@
 
       <ul class="collapsible" data-collapsible="accordion">
     <li>
-      <div class="collapsible-header"><i class="material-icons">¿Qué es DePrevia?</i>First</div>
+      <div class="collapsible-header"><i class="material-icons">library_books</i>¿Qué es DePrevia?</div>
       <div class="collapsible-body"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita mollitia facilis deserunt placeat nesciunt itaque velit, consequatur consectetur, dolorem aliquid voluptas! Quia excepturi odio pariatur ipsam sapiente ea. Excepturi, deleniti!</span></div>
     </li>
     <li>
-      <div class="collapsible-header"><i class="material-icons">¿Cómo puedo crear un grupo?</i>Second</div>
+      <div class="collapsible-header"><i class="material-icons">library_books</i>¿Cómo puedo crear un grupo?</div>
       <div class="collapsible-body"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita mollitia facilis deserunt placeat nesciunt itaque velit, consequatur consectetur, dolorem aliquid voluptas! Quia excepturi odio pariatur ipsam sapiente ea. Excepturi, deleniti!</span></div>
     </li>
     <li>
-      <div class="collapsible-header"><i class="material-icons">¿Dónde aparece mi grupo?</i>Third</div>
+      <div class="collapsible-header"><i class="material-icons">library_books</i>¿Dónde aparece mi grupo?</div>
       <div class="collapsible-body"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita mollitia facilis deserunt placeat nesciunt itaque velit, consequatur consectetur, dolorem aliquid voluptas! Quia excepturi odio pariatur ipsam sapiente ea. Excepturi, deleniti!</span></div>
     </li>
     <li>
-      <div class="collapsible-header"><i class="material-icons">¿Mi grupo es visible para todos?</i>First</div>
+      <div class="collapsible-header"><i class="material-icons">library_books</i>¿Mi grupo es visible para todos?</div>
       <div class="collapsible-body"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita mollitia facilis deserunt placeat nesciunt itaque velit, consequatur consectetur, dolorem aliquid voluptas! Quia excepturi odio pariatur ipsam sapiente ea. Excepturi, deleniti!</span></div>
     </li>
     <li>
-      <div class="collapsible-header"><i class="material-icons">¿Cuántos integrantes puede tener un grupo?</i>First</div>
+      <div class="collapsible-header"><i class="material-icons">library_books</i>¿Cuántos integrantes puede tener un grupo?</div>
       <div class="collapsible-body"><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita mollitia facilis deserunt placeat nesciunt itaque velit, consequatur consectetur, dolorem aliquid voluptas! Quia excepturi odio pariatur ipsam sapiente ea. Excepturi, deleniti!</span></div>
     </li>
   </ul>
-
     </div>
     <br><br>
   </div>
+
+
+
 
 <?php require('templates/footer.php'); ?>
 
