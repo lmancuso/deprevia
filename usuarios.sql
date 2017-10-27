5f2d25145d76e438e2ee9b6e6f8063272c5d0dc5
franchinna <francinnante@gmail.com>
2017-10-26 22:00:43 -0300

    Se muda a objetos y se sube MySQL

---
+++ usuarios.sql
@@ -0,0 +1,5 @@
+select *
+from usuarios;
+
+INSERT INTO usuarios (first_name, last_name, username,password,email,genero,edad)
+VALUES ("Franco","Franco","Franco","Franco","Franco","Franco","Franco");
